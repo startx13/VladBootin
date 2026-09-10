02d5726c6490d040ff2ab61b8b9045be5753c444
startx13 <rossileonardo57@gmail.com>
2026-09-10 23:00:48 +0200

    refactor: update UART clock frequency to 48MHz and baudrate to 3Mbps

--- Image_Loader/image.py
+++ Image_Loader/image.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 
 SERIAL_PORT = "/dev/pts/4"
-BAUDRATE = 115200
+BAUDRATE = 3000000
 
 KERNEL_PATH = Path("kernel.img")
 DTB_PATH = Path("bcm2709-rpi-2-b.dtb")
--- lib/uart.c
+++ lib/uart.c
@@ -4,138 +4,290 @@
 #include "printf.h"
 
 #ifndef GPFSEL1
-#define GPFSEL1 0x3F200004 // Indirizzo fisico GPIO Function Select 1 su RPi 2 (BCM2836)
+#define GPFSEL1 0x3F200004 // GPIO Function Select 1 - RPi 2 / BCM2836
 #endif
- 
+
 // Memory-Mapped I/O output
 static inline void mmio_write(uint32_t reg, uint32_t data)
 {
     *(volatile uint32_t*)reg = data;
 }
- 
+
 // Memory-Mapped I/O input
 static inline uint32_t mmio_read(uint32_t reg)
 {
     return *(volatile uint32_t*)reg;
 }
- 
-// Loop <delay> times in a way that the compiler won't optimize away
+
+// Delay loop
 static inline void delay(int32_t count)
 {
-    asm volatile("__delay_%=: subs %[count], %[count], #1; bne __delay_%=\n"
-         : "=r"(count): [count]"0"(count) : "cc");
+    asm volatile(
+        "__delay_%=: subs %[count], %[count], #1; "
+        "bne __delay_%=\n"
+        : "=r"(count)
+        : [count]"0"(count)
+        : "cc"
+    );
 }
- 
- 
-// A Mailbox message with set clock rate of PL011 to 3MHz tag
-volatile unsigned int  __attribute__((aligned(16))) uart_mbox[9] = {
-    9*4, 0, 0x38002, 12, 8, 2, 3000000, 0 ,0
+
+/*
+ * Mailbox message:
+ *
+ * Set PL011 UART clock to 48 MHz.
+ *
+ * Mailbox property tag:
+ *   0x38002 = Set Clock Rate
+ *   clock ID 2 = UART / PL011
+ *
+ * With UARTCLK = 48 MHz and:
+ *
+ *   IBRD = 1
+ *   FBRD = 0
+ *
+ * the resulting baudrate is:
+ *
+ *   48,000,000 / (16 * 1) = 3,000,000 baud
+ */
+volatile unsigned int __attribute__((aligned(16))) uart_mbox[9] = {
+    9 * 4,       // Buffer size
+    0,            // Request
+    0x38002,      // Set Clock Rate
+    12,           // Value buffer size
+    8,            // Request/response size
+    2,            // Clock ID: PL011 UART
+    48000000,     // Clock rate: 48 MHz
+    0,            // End tag
+    0
 };
- 
+
 void uart_init()
 {
-    // 1. Forza il clock della periferica UART tramite Mailbox a 3MHz
-    // Attendi che la mailbox sia pronta per scrivere
-    while (mmio_read(MBOX_STATUS) & 0x80000000) { }
-    // Scrivi l'indirizzo del buffer (canale 8 per le proprietà tag)
-    mmio_write(MBOX_WRITE, ((uint32_t)((void*)&uart_mbox) & ~0xF) | 8);
-    
-    // Attendi la risposta
-    while (1) {
-        while (mmio_read(MBOX_STATUS) & 0x40000000) { } // Attendi dati in lettura
-        if (((mmio_read(MBOX_READ) & 0xF) == 8)) break; // Verifica sia il canale 8
+    /*
+     * 1. Set PL011 UART clock to 48 MHz through mailbox.
+     */
+
+    // Wait until mailbox can accept a write
+    while (mmio_read(MBOX_STATUS) & 0x80000000) {
     }
 
+    // Send buffer address on property channel 8
+    mmio_write(
+        MBOX_WRITE,
+        ((uint32_t)((void*)&uart_mbox) & ~0xF) | 8
+    );
+
+    // Wait for mailbox response
+    while (1) {
+        while (mmio_read(MBOX_STATUS) & 0x40000000) {
+        }
+
+        uint32_t response = mmio_read(MBOX_READ);
+
+        if ((response & 0xF) == 8)
+            break;
+    }
+
+    /*
+     * 2. Disable UART while configuring it.
+     */
     mmio_write(UART0_CR, 0x00000000);
 
-    // Legge il registro GPFSEL1
+    /*
+     * 3. Configure GPIO 14/15 for ALT0 = PL011 UART.
+     */
+
     uint32_t selector = mmio_read(GPFSEL1);
-    selector &= ~((7 << 12) | (7 << 15)); // Reset bit per GPIO 14 e 15
-    selector |= (4 << 12) | (4 << 15);   // Imposta ALT0 (100b) per UART0
+
+    // Clear GPIO14 and GPIO15 function bits
+    selector &= ~((7 << 12) | (7 << 15));
+
+    // ALT0 = 100b
+    selector |= (4 << 12) | (4 << 15);
+
     mmio_write(GPFSEL1, selector);
 
-    // Gestione Pull-up/down
+    /*
+     * 4. Disable GPIO pull-up/down.
+     */
+
     mmio_write(GPPUD, 0x00000000);
+
     delay(150);
-    mmio_write(GPPUDCLK0, (1 << 14) | (1 << 15));
+
+    mmio_write(
+        GPPUDCLK0,
+        (1 << 14) | (1 << 15)
+    );
+
     delay(150);
+
     mmio_write(GPPUDCLK0, 0x00000000);
 
-    // Pulisci interrupt pendenti ed errori
+    /*
+     * 5. Clear pending interrupts and UART errors.
+     */
+
     mmio_write(UART0_ICR, 0x7FF);
     mmio_write(UART0_RSRECR, 0x0);
 
-    // Configurazione Baud Rate per Clock UART a 3MHz (RPi 2 Default)
+    /*
+     * 6. Configure baudrate.
+     *
+     * UARTCLK = 48 MHz
+     *
+     * Baud = UARTCLK / (16 * divisor)
+     *
+     * divisor = IBRD + FBRD / 64
+     *
+     * IBRD = 1
+     * FBRD = 0
+     *
+     * Baud = 48,000,000 / 16
+     *      = 3,000,000 baud
+     */
+
     mmio_write(UART0_IBRD, 1);
-    mmio_write(UART0_FBRD, 40);
+    mmio_write(UART0_FBRD, 0);
 
-    // 8N1 e abilita FIFO
-    mmio_write(UART0_LCRH, (1 << 4) | (1 << 5) | (1 << 6));
+    /*
+     * 7. Configure:
+     *
+     *   8 data bits
+     *   No parity
+     *   1 stop bit
+     *   FIFO enabled
+     */
 
-    // Maschera interrupt
-    mmio_write(UART0_IMSC, (1 << 1) | (1 << 4) | (1 << 5) | (1 << 6) |
-                           (1 << 7) | (1 << 8) | (1 << 9) | (1 << 10));
+    mmio_write(
+        UART0_LCRH,
+        (1 << 4) |  // FEN
+        (1 << 5) |  // WLEN bit 0
+        (1 << 6)    // WLEN bit 1
+    );
 
-    // Abilita UART, TX e RX
-    mmio_write(UART0_CR, (1 << 0) | (1 << 8) | (1 << 9));
+    /*
+     * 8. Mask interrupts.
+     */
+
+    mmio_write(
+        UART0_IMSC,
+        (1 << 1)  |  // CTSMIM
+        (1 << 4)  |  // RXIM
+        (1 << 5)  |  // TXIM
+        (1 << 6)  |  // RTIM
+        (1 << 7)  |  // FEIM
+        (1 << 8)  |  // PEIM
+        (1 << 9)  |  // BEIM
+        (1 << 10)    // OEIM
+    );
+
+    /*
+     * 9. Enable UART, TX and RX.
+     */
+
+    mmio_write(
+        UART0_CR,
+        (1 << 0) |   // UARTEN
+        (1 << 8) |   // TXE
+        (1 << 9)     // RXE
+    );
 }
- 
+
 void uart_putc(unsigned char c)
 {
-    // Wait for UART to become ready to transmit.
-    while ( mmio_read(UART0_FR) & (1 << 5) ) { }
+    // Wait until TX FIFO is not full
+    while (mmio_read(UART0_FR) & (1 << 5)) {
+    }
+
     mmio_write(UART0_DR, c);
 }
- 
+
 unsigned char uart_getc()
 {
-    // Attendi che la RX FIFO contenga dati
-    while ( mmio_read(UART0_FR) & (1 << 4) ) { }
-    
-    // Pulisci errori di linea accumulati
+    // Wait until RX FIFO contains data
+    while (mmio_read(UART0_FR) & (1 << 4)) {
+    }
+
+    // Clear accumulated receive errors
     mmio_write(UART0_RSRECR, 0);
-    
+
     return (unsigned char)(mmio_read(UART0_DR) & 0xFF);
 }
- 
+
 void uart_puts(const char* str)
 {
-    for (size_t i = 0; str[i] != '\0'; i ++)
+    for (size_t i = 0; str[i] != '\0'; i++) {
         uart_putc((unsigned char)str[i]);
+    }
 }
- 
-void uart_hex(unsigned int d) {
+
+void uart_hex(unsigned int d)
+{
     unsigned int n;
     int c;
-    for(c=28;c>=0;c-=4) {
-        // get highest tetrad
-        n=(d>>c)&0xF;
-        // 0-9 => '0'-'9', 10-15 => 'A'-'F'
-        n+=n>9?0x37:0x30;
+
+    for (c = 28; c >= 0; c -= 4) {
+        n = (d >> c) & 0xF;
+
+        n += n > 9 ? 0x37 : 0x30;
+
         uart_putc(n);
     }
 }
 
-void uart_dump(unsigned int ptr,unsigned int size)
+void uart_dump(unsigned int ptr, unsigned int size)
 {
-    printf("\r\nDumping memory at 0x%x size 0x%x\r\n",ptr,size-ptr);
-    unsigned long a,b,d;
+    printf(
+        "\r\nDumping memory at 0x%x size 0x%x\r\n",
+        ptr,
+        size - ptr
+    );
+
+    unsigned long a, b, d;
     unsigned char c;
-    for(a=ptr;a<size;a+=16)
-    {
-        uart_hex(a); uart_puts(": ");
-        for(b=0;b<16;b++) {
-            c=*((unsigned char*)(a+b));
-            d=(unsigned int)c;d>>=4;d&=0xF;d+=d>9?0x37:0x30;uart_putc(d);
-            d=(unsigned int)c;d&=0xF;d+=d>9?0x37:0x30;uart_putc(d);
+
+    for (a = ptr; a < size; a += 16) {
+
+        uart_hex(a);
+        uart_puts(": ");
+
+        /*
+         * Hexadecimal dump
+         */
+        for (b = 0; b < 16; b++) {
+
+            c = *((unsigned char*)(a + b));
+
+            d = (unsigned int)c;
+            d >>= 4;
+            d &= 0xF;
+            d += d > 9 ? 0x37 : 0x30;
+            uart_putc(d);
+
+            d = (unsigned int)c;
+            d &= 0xF;
+            d += d > 9 ? 0x37 : 0x30;
+            uart_putc(d);
+
             uart_putc(' ');
-            if(b%4==3)
+
+            if (b % 4 == 3)
                 uart_putc(' ');
         }
-        for(b=0;b<16;b++) {
-            c=*((unsigned char*)(a+b));
-            uart_putc(c<32||c>=127?'.':c);
+
+        /*
+         * ASCII dump
+         */
+        for (b = 0; b < 16; b++) {
+
+            c = *((unsigned char*)(a + b));
+
+            uart_putc(
+                c < 32 || c >= 127 ? '.' : c
+            );
         }
+
         uart_putc('\r');
         uart_putc('\n');
     }
